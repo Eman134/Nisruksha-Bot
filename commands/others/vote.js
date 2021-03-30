bc26d60d547984724b55a1b69083c0d2c1464b98
Eman134 <kaykeeman@gmail.com>
2021-03-30 13:58:33 -0300

    Update vote.js

--- commands/others/vote.js
+++ commands/others/vote.js
@@ -14,7 +14,7 @@ module.exports = {
                 .setColor('#36393f')
                 .setAuthor(msg.author.tag, msg.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
                 .setDescription('Votando no bot você nos ajudará com o crescimento do mesmo, além de você também ser recompensado!')
-                //.addField('**Zuraaa**', `🗳 [Clique aqui](https://zuraaa.com/bots/763815343507505183/)\n**Recompensas:**\n1x 📦 Caixa Comum`)
+                .addField('**Zuraaa**', `🗳 [Clique aqui](https://zuraaa.com/bots/763815343507505183/)\n**Recompensas:**\n1x 📦 Caixa Comum`)
                 .addField('**Top.gg**', `🗳 [Clique aqui](https://top.gg/bot/763815343507505183)\n**Recompensas:**\n1x ${API.money2} ${API.money2emoji}`)
                 msg.quote(embed);
 
