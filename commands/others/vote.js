1e2f8e43c57863b62500e25eafa8234ba1d15888
Eman134 <kaykeeman@gmail.com>
2021-06-27 16:55:56 -0300

    0.2.1.7-57

--- commands/others/vote.js
+++ commands/others/vote.js
@@ -12,44 +12,13 @@ module.exports = {
         const check1 = await API.playerUtils.cooldown.check(msg.author, "votetopgg");
         if (check1) votedtopgg = true
 
-        const { best } = require("../../_classes/config");
-
-        const https = require('https')
-        const options = {
-            hostname: 'bestlist.online',
-            port: 443,
-            path: '/api/users/voted/' + msg.author.id,
-            method: 'GET',
-            headers: {
-            'Content-Type': 'application/json',
-            'Authorization': best.token
-          }
-        }
-
-        let votedbest = false
-
-        const req = https.request(options, res => {
-            
-            res.on('data', d => {
-                d = JSON.parse(d.toString());
-                votedbest = d.votedToday
-                const embed = new Discord.MessageEmbed()
-                .setColor('#36393f')
-                .setAuthor(msg.author.tag, msg.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
-                .setDescription('Votando no bot você nos ajudará com o crescimento do mesmo, além de você também ser recompensado!')
-                .addField( (votedbest ? '🔴' : '🟢') + ' **Best**', `🗳 [Clique aqui](https://www.bestlist.online/bots/763815343507505183)\n**Recompensas:**\n1x 📦 Caixa Comum`)
-                .addField( (votedtopgg ? '🔴' : '🟢') + ' **Top.gg**', `🗳 [Clique aqui](https://top.gg/bot/763815343507505183)\n**Recompensas:**\n1x ${API.money2} ${API.money2emoji}`)
-                msg.quote({ embeds: [embed]});
-
-            })
-
-        })
-
-            req.on('error', error => {
-            console.error(error)
-        })
-
-        req.end()
+        const embed = new Discord.MessageEmbed()
+            .setColor('#36393f')
+            .setAuthor(msg.author.tag, msg.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+            .setDescription('Votando no bot você nos ajudará com o crescimento do mesmo, além de você também ser recompensado!')
+            .addField(' **Best**', `🗳 [Clique aqui](https://www.bestlist.online/bots/763815343507505183)\n**Recompensas:**\n1x 📦 Caixa Comum`)
+            .addField( (votedtopgg ? '🔴' : '🟢') + ' **Top.gg**', `🗳 [Clique aqui](https://top.gg/bot/763815343507505183)\n**Recompensas:**\n1x ${API.money2} ${API.money2emoji}`)
+        await msg.quote({ embeds: [embed]});
         
 	}
 };
