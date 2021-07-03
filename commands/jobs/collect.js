567de6c42f3d65ce456bb7a39bc50f53b40cdca8
Eman134 <kaykeeman@gmail.com>
2021-07-03 13:21:47 -0300

    Update collect.js

--- commands/jobs/collect.js
+++ commands/jobs/collect.js
@@ -154,7 +154,7 @@ module.exports = {
                     if (b.customID == 'stopBtn') {
                         reacted = true;
                         collector.stop();
-                        b.deferUpdate().catch()
+                        b.deferUpdate().catch() 
                     }
                 });
 
