18fe53df3d109e729511545efb0db65d254dfb15
Eman134 <kaykeeman@gmail.com>
2021-10-09 16:22:43 -0300

    Update error.js

--- events/error.js
+++ events/error.js
@@ -4,7 +4,7 @@ module.exports = {
     execute: async (API, err) => {
 
         const Discord = API.Discord;
-        let channel = API.client.channels.cache.get('769757806835531827')
+        let channel = API.client.channels.cache.get('896477669913403403')
         const embed = new Discord.MessageEmbed()
             .setColor('#b8312c')
             .setTitle('<:error:736274027756388353> Um erro foi encontrado')
